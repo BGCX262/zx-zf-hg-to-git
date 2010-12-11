7738c332cc6e28b070b26dc48901b70e2ffdc84a
avd <coder4web@gmail.com>
2010-12-11 09:32:52 +0300

    ini update

--- application/config_ini.php
+++ application/config_ini.php
@@ -5,7 +5,11 @@ define('UID', uniqid(getmypid()));
 
 #echo 'DEBUG:<br><textarea rows=10 cols=100>' . print_r(get_include_path(), 1) . '</textarea><br>'; die;
 #echo 'DEBUG:<br><textarea rows=10 cols=100>' . print_r(getcwd(), 1) . '</textarea><br>'; die;
-set_include_path($root . 'library'); // for early Zend_Logger loading
+if (!defined('CMS'))
+{
+	set_include_path($root . 'library'); // for early Zend_Logger loading
+	function __autoload($path) {include str_replace('_','/',$path) . '.php'; return $path;} // $autoloader->setDefaultAutoloader(create_function('$class', "include str_replace('_', '/', \$class) . '.php';"));
+}
 
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
@@ -14,7 +18,7 @@ ini_set('html_errors', 0);
 //--< setup location and debug
 if (!defined('LOCATION'))
 {
-	if ( ($_SERVER['SERVER_ADDR'] == '192.168.0.118') || (strpos($_SERVER['SERVER_NAME'], '.lh') !== false) ) { // devel local
+	if ( (strpos($_SERVER['SERVER_NAME'], '.lh') !== false) ||  in_array($_SERVER['SERVER_ADDR'], array('127.0.0.1', '192.168.0.118')) ) { // devel local
 		define('LOCATION', 'devel');
 		define('LOCALHOST', 1);
 	} elseif ( strpos($_SERVER['SERVER_NAME'], 'test') !== false ) { // devel server
@@ -26,5 +30,3 @@ if (!defined('LOCATION'))
 	}
 }
 //-->
-
-function __autoload($path) {include str_replace('_','/',$path) . '.php'; return $path;} // $autoloader->setDefaultAutoloader(create_function('$class', "include str_replace('_', '/', \$class) . '.php';"));
